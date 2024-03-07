e104cb7f659831d21b10d3e51379f63dfdff0016
Julian Lopez <a20490712@itmexicali.edu.mx>
2024-03-07 10:14:28 -0800

    Se agrego flex 0 0 auto

--- frontend/src/views/Login.jsx
+++ frontend/src/views/Login.jsx
@@ -34,9 +34,8 @@ const Login = () => {
     return (
         <Group gap={0} align="center" justify="center" w="100vw" mih="100vh" wrap="nowrap">
         <Stack justify="center" align="center" bg="#6C5F5B" h="100vh" w="50%">
-            <Image w="92%" h="92%" radius="md"
-            src="https://cdn-3.expansion.mx/dims4/default/67914fa/2147483647/strip/true/crop/1638x2048+0+0/resize/1200x1500!/format/webp/quality/60/?url=https%3A%2F%2Fcdn-3.expansion.mx%2Fb5%2Fa9%2F148bd3ba4ab780899ecd12d46cb7%2Fsalon-gallos-merida.jpg"/>
-        </Stack>,
+            <Image w="92%" h="92%" radius="md" flex="0 0 auto" src="https://cdn-3.expansion.mx/dims4/default/67914fa/2147483647/strip/true/crop/1638x2048+0+0/resize/1200x1500!/format/webp/quality/60/?url=https%3A%2F%2Fcdn-3.expansion.mx%2Fb5%2Fa9%2F148bd3ba4ab780899ecd12d46cb7%2Fsalon-gallos-merida.jpg"/>
+        </Stack>
         <Stack justify="center" align="center" bg="#F6F1EE" h="100vh" w="50%">
             <Title order={1} fw={700} size="3.5rem">¡Bienvenido!</Title>
             <Text size="lg">Inicia sesión en AltaServe</Text>
