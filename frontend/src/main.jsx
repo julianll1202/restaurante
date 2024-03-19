984f9d365968c210e4fcb140bc5c04d40679ceb0
Alexis Peñuñuri <alxpesa@gmail.com>
2024-03-18 23:46:05 -0700

    Limpieza de login y ajustes al tema

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -60,7 +60,7 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MantineProvider theme={restauranteTheme}   >
+    <MantineProvider theme={restauranteTheme} withGlobalStyles>
         <AuthProvider>
           <RouterProvider router={router} />
 
