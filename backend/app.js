2b5753d60c13633d694bd222bd5fbe59e6c0961d
Jesus Manuel Lizarraga Sabedra <a20490710@itmexicali.edu.mx>
2024-03-20 16:26:29 -0700

    Avance SOAP implementacion

--- backend/app.js
+++ backend/app.js
@@ -14,8 +14,8 @@ import comprasRouter from './routes/compras.js'
 import productosRouter from './routes/productos.js'
 import rolesRouter from './routes/roles.js'
 import imgRouter from './routes/imagenes.js'
-
 import cors from 'cors'
+import {soap} from 'express-soap';
 
 const app = express()
 
@@ -38,5 +38,121 @@ app.use('/compras', comprasRouter)
 app.use('/productos', productosRouter)
 app.use('/roles', rolesRouter)
 app.use('/imagenes', imgRouter)
+
+
+app.use('/soap/calculation', soap({
+    services: {
+        CalculatorService: {
+            Calculator: {
+                Add({a, b}, res) {
+                    res({
+                            result: a + b
+                    });
+                },
+                Subtract({a, b}, res) {
+                    res({
+                            result: a - b
+                    });
+                }
+              }
+        }
+    }, 
+
+    wsdl: `<definitions
+    xmlns="http://schemas.xmlsoap.org/wsdl/"
+    xmlns:soap="http://schemas.xmlsoap.org/wsdl/soap/"
+    xmlns:tns="http://localhost:3000/soap/calculation"
+    xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+    name="CalculatorService"
+    targetNamespace="http://localhost:3000/soap/calculation">
+
+    <types>
+        <schema xmlns="http://www.w3.org/2001/XMLSchema" targetNamespace="http://localhost:3000/soap/calculation">
+            <element name="AddRequest">
+                <complexType>
+                    <sequence>
+                        <element name="a" type="xsd:int"/>
+                        <element name="b" type="xsd:int"/>
+                    </sequence>
+                </complexType>
+            </element>
+            <element name="AddResponse">
+                <complexType>
+                    <sequence>
+                        <element name="result" type="xsd:int"/>
+                    </sequence>
+                </complexType>
+            </element>
+            <element name="SubtractRequest">
+                <complexType>
+                    <sequence>
+                        <element name="a" type="xsd:int"/>
+                        <element name="b" type="xsd:int"/>
+                    </sequence>
+                </complexType>
+            </element>
+            <element name="SubtractResponse">
+                <complexType>
+                    <sequence>
+                        <element name="result" type="xsd:int"/>
+                    </sequence>
+                </complexType>
+            </element>
+        </schema>
+    </types>
+
+    <message name="AddRequest">
+        <part name="parameters" element="tns:AddRequest"/>
+    </message>
+    <message name="AddResponse">
+        <part name="parameters" element="tns:AddResponse"/>
+    </message>
+    <message name="SubtractRequest">
+        <part name="parameters" element="tns:SubtractRequest"/>
+    </message>
+    <message name="SubtractResponse">
+        <part name="parameters" element="tns:SubtractResponse"/>
+    </message>
+
+    <portType name="CalculatorPortType">
+        <operation name="Add">
+            <input message="tns:AddRequest"/>
+            <output message="tns:AddResponse"/>
+        </operation>
+        <operation name="Subtract">
+            <input message="tns:SubtractRequest"/>
+            <output message="tns:SubtractResponse"/>
+        </operation>
+    </portType>
+
+    <binding name="CalculatorBinding" type="tns:CalculatorPortType">
+        <soap:binding style="document" transport="http://schemas.xmlsoap.org/soap/http"/>
+        <operation name="Add">
+            <soap:operation soapAction="http://localhost:3000/soap/calculation/Add"/>
+            <input>
+                <soap:body use="literal"/>
+            </input>
+            <output>
+                <soap:body use="literal"/>
+            </output>
+        </operation>
+        <operation name="Subtract">
+            <soap:operation soapAction="http://localhost:3000/soap/calculation/Subtract"/>
+            <input>
+                <soap:body use="literal"/>
+            </input>
+            <output>
+                <soap:body use="literal"/>
+            </output>
+        </operation>
+    </binding>
+
+    <service name="CalculatorService">
+        <port name="CalculatorPort" binding="tns:CalculatorBinding">
+            <soap:address location="http://localhost:3000/soap/calculation"/>
+        </port>
+    </service>
+</definitions>` // or xml (both options are valid)
+}));
 
 export default app
