bb2cd0e93c81ee14220cd3eb4ab643f54df2f3a5
Jesus Manuel Lizarraga Sabedra <a20490710@itmexicali.edu.mx>
2024-04-06 18:50:00 -0700

    Avance SOAP Cliente

--- backend/routes/mesas.js
+++ backend/routes/mesas.js
@@ -1,67 +1,57 @@
 import express from 'express'
-import { createMesa, deleteMesa, getAllMesas, getMesasLibres, updateEstatusMesa, updateMesa } from '../controllers/mesaController.js'
 var soap = require('soap');
 
 const router = express.Router()
 
-router.post('/listar', async function (req, res) {
-    /*
-    const mesas = await getAllMesas(req, res)
-    res.status(200).send(mesas)
-    */
+router.get('/listar', async function (req, res) {
     var args = {};
     var xml = 'C:/Users/JML15/Desktop/CODE/ProyectoWEBYAOS/restaurante/backend/myservice.wsdl'
-
     try {
         var client = await soap.createClientAsync(xml, { endpoint: 'http://localhost:3000/soap/mesas'});
         var result = await client.ListarAsync(args);
-        console.log(result[0])
-        res.status(200).send(result)
+        res.status(200).send(result[0])
     } catch (error) {
         console.log(error);
         res.status(400).send('Error al listar las mesas')
     }
-    
-})
-
-router.get('/libres', async function (req, res) {
-    const mesas = await getMesasLibres()
-    res.status(200).send(mesas)
 })
 
 router.post('/crear', async function (req, res) {
-    const mesa = await createMesa(req, res)
-    if (!JSON.stringify(mesa).startsWith('"Error')) {
-        res.status(200).send(mesa)
-    } else {
-        res.status(400).send(mesa)
+    const {capacidad, ubicacion, tipoMesa} = req.body;
+    var xml = 'C:/Users/JML15/Desktop/CODE/ProyectoWEBYAOS/restaurante/backend/myservice.wsdl'
+    try {
+        var client = await soap.createClientAsync(xml, { endpoint: 'http://localhost:3000/soap/mesas'});
+        var result = await client.CrearAsync({capacidad, ubicacion, tipoMesa});
+        res.status(200).send(result[0])
+    } catch (error) {
+        console.log(error);
+        res.status(400).send('Error al crear la mesa')
     }
 })
 
 router.put('/actualizar', async function (req, res) {
-    const mesaActualizada = await updateMesa(req, res)
-    if (!JSON.stringify(mesaActualizada).startsWith('"Error')) {
-        res.status(200).send(mesaActualizada)
-    } else {
-        res.status(400).send(mesaActualizada)
-    }
-})
-
-router.put('/cambiar-estado', async function (req, res) {
-    const mesaActualizada = await updateEstatusMesa(req, res)
-    if (!JSON.stringify(mesaActualizada).startsWith('"Error')) {
-        res.status(200).send(mesaActualizada)
-    } else {
-        res.status(400).send(mesaActualizada)
+    const {mesaId, capacidad, ubicacion, tipoMesa, ocupada} = req.body;
+    var xml = 'C:/Users/JML15/Desktop/CODE/ProyectoWEBYAOS/restaurante/backend/myservice.wsdl'
+    try {
+        var client = await soap.createClientAsync(xml, { endpoint: 'http://localhost:3000/soap/mesas'});
+        var result = await client.ActualizarAsync({mesaId, capacidad, ubicacion, tipoMesa, ocupada});
+        res.status(200).send(result[0])
+    } catch (error) {
+        console.log(error);
+        res.status(400).send('Error al actualizar la mesa')
     }
 })
 
 router.delete('/eliminar', async function (req, res) {
-    const mesaD = await deleteMesa(req, res)
-    if (!JSON.stringify(mesaD).startsWith('"Error')) {
-        res.status(200).send(mesaD)
-    } else {
-        res.status(400).send(mesaD)
+    const {mesaId} = req.body;
+    var xml = 'C:/Users/JML15/Desktop/CODE/ProyectoWEBYAOS/restaurante/backend/myservice.wsdl'
+    try {
+        var client = await soap.createClientAsync(xml, { endpoint: 'http://localhost:3000/soap/mesas'});
+        var result = await client.EliminarAsync({mesaId});
+        res.status(200).send(result[0])
+    } catch (error) {
+        console.log(error);
+        res.status(400).send('Error al eliminar la mesa')
     }
 })
 export default router
